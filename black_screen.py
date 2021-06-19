6b346bcfc98722c2507b39aa928d76466bace3bb
Snigdha_13 <65667637+Snigdha-Pothuri@users.noreply.github.com>
2021-06-19 15:48:49 +0530

    Add files via upload

--- black_screen.py
+++ black_screen.py
@@ -11,16 +11,16 @@ for i in range(0,60):
 bg=np.flip(bg,axis=1)
 while (capture.isOpened()):
   ret,img=capture.read()
-  if not ret:
+  if not ret: 
     break
   img=np.flip(img,axis=1)
   hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
   l_black=np.array([30,30,0])
   u_black=np.array([104,153,70])
-  mask_1=cv2.inRange(hsv,lower_red,upper_red)
+  mask_1=cv2.inRange(hsv,l_black,u_black)
   l_black=np.array([30,30,0])
   u_black=np.array([104,153,70])
-  mask_2=cv2.inRange(hsv,lower_red,upper_red)
+  mask_2=cv2.inRange(hsv,l_black,u_black)
   mask_1=mask_1+mask_2
   mask_1=cv2.morphologyEx(mask_1,cv2.MORPH_OPEN,np.ones((3,3),np.uint8))
   mask_1=cv2.morphologyEx(mask_1,cv2.MORPH_DILATE,np.ones((3,3),np.uint8))
